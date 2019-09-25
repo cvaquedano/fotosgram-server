ca1fe84dfdc568a0af260e807437ffca97cf141a
Carlos Vaquedano <chvb2002@hotmail.com>
2019-09-25 12:53:01 -0600

    inicial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import Server from "./classes/server";
+import userRoutes from "./routes/usuario";
+
+
+const server = new Server();
+
+server.app.use( '/user', userRoutes )
+
+server.start(()=>{
+    console.log(`Servidor corriendo en puerto ${server.port} `);
+});
