a311e0437abe1b51aa9662416cb8e05bc942ece1
Carlos Vaquedano <chvb2002@hotmail.com>
2019-09-26 07:39:28 -0600

    encriptacion de contraseña

--- index.ts
+++ index.ts
@@ -1,10 +1,27 @@
 import Server from "./classes/server";
 import userRoutes from "./routes/usuario";
-
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 
 const server = new Server();
 
+server.app.use( bodyParser.urlencoded({
+    extended:true
+}));
+
+server.app.use( bodyParser.json());
+
 server.app.use( '/user', userRoutes )
+
+mongoose.connect('mongodb://localhost:27017/fotosgram',{
+    useNewUrlParser: true,
+    useCreateIndex: true
+}, (err) =>{
+    if (err ) throw err;
+
+    console.log('base de  datos online');
+
+})
 
 server.start(()=>{
     console.log(`Servidor corriendo en puerto ${server.port} `);
